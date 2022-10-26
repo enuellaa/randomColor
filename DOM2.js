69386705df78efd6139976848af24635c14f4839
enuella <ellaeewurama@gmail.com>
2022-10-26 14:26:34 +0200

    first commit

---
+++ DOM2.js
@@ -0,0 +1,19 @@
+function setcl1() {
+    const colorValue = document.querySelector("#Color").value;
+    document.body.style.backgroundColor = colorValue;
+    document.querySelector("#Color").value = ""
+
+    document.querySelector("#colorname").innerHTML = colorValue;
+}
+
+
+
+
+function randomcl1() {
+    const rnNumber = Math.floor(Math.random() * colorList.length)
+
+    document.getElementById("colorbody").style.backgroundColor = colorList[rnNumber];
+    document.querySelector("#colorname").innerHTML = colorList[rnNumber];
+}
+
+const colorList = ["AliceBlue", "AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "Black", "BlanchedAlmond", "Blue", "BlueViolet", "Brown", "BurlyWood", "CadetBlue", "Chartreuse", "Chocolate", "Coral", "CornflowerBlue", "Cornsilk", "Crimson", "Cyan", "DarkBlue", "DarkCyan", "DarkGoldenRod", "DarkGray", "DarkGrey", "DarkGreen", "DarkKhaki", "DarkMagenta", "DarkOliveGreen", "DarkOrange", "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen", "DarkSlateBlue", "DarkSlateGray", "DarkSlateGrey", "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue", "DimGray", "DimGrey", "DodgerBlue", "FireBrick", "FloralWhite", "ForestGreen", "Fuchsia", "Gainsboro", "GhostWhite", "Gold", "GoldenRod", "Gray", "Grey", "Green", "GreenYellow", "HoneyDew", "HotPink", "IndianRed", "Indigo", "Ivory", "Khaki", "Lavender", "LavenderBlush", "LawnGreen", "LemonChiffon", "LightBlue", "LightCoral", "LightCyan", "LightGoldenRodYellow", "LightGray", "LightGrey", "LightGreen", "LightPink", "LightSalmon", "LightSeaGreen", "LightSkyBlue", "LightSlateGray", "LightSlateGrey"]
